c840c11b06d9c278fe6e7d4278453b30dbf95a30
Efrin González <efringonzalez@gmail.com>
2017-09-12 14:34:27 +0200

    Unit Testing HomeController
    First test in place for the HomeController.

--- EmployeesContol/src/test/java/com/employees/control/AppTests.java
+++ EmployeesContol/src/test/java/com/employees/control/AppTests.java
@@ -1,16 +1,24 @@
 package com.employees.control;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.employees.control.Controller.HomeController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AppTests {
 
 	@Test
-	public void contextLoads() {
+	public void isHomeControllerRunning() {
+		HomeController homeCtrl = new HomeController();
+		String result = homeCtrl.home();
+		assertEquals(result, "Employee Control Reporter");
+		
 	}
 
 }
