2d79f83c11a1259229ca5d00be5a296b2740eb91
Efrin González <efringonzalez@gmail.com>
2017-09-14 17:24:30 +0200

    Testing employee controller
    Testin not null and the list has the correct size.

--- EmployeesContol/src/test/java/com/employees/control/EmployeeControllerTest.java
+++ EmployeesContol/src/test/java/com/employees/control/EmployeeControllerTest.java
@@ -1,12 +1,14 @@
 package com.employees.control;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
 import com.employees.control.Controller.EmployeeStub;
 import com.employees.control.Model.Employee;
 
@@ -18,7 +20,12 @@ public class EmployeeControllerTest {
 		EmployeeStub es = new EmployeeStub();
 		List<Employee> list = new ArrayList<Employee>();
 		list = EmployeeStub.list();
-		//assert that the list is not null and that is equal to four elements
+		
+		//assert that the list is not null 
+		assertNotNull(list);
+		//and that is equal to four elements
+		assertThat(list.size(), is(4));
+		
 	}
 
 }
